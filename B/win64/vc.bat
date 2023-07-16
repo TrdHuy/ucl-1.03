f5d54a8856eda5d4538467d69c7c48b9cf81b342
TrdHuy <32306489+TrdHuy@users.noreply.github.com>
2023-07-16 19:09:22 +0700

    first commit

---
+++ B/win64/vc.bat
@@ -0,0 +1,47 @@
+@echo // Copyright (C) 1996-2004 Markus F.X.J. Oberhumer
+@echo //
+@echo //   Windows 64-bit (AMD64 or Itanium)
+@echo //   Microsoft Visual C/C++
+@echo //
+@call b\prepare.bat
+@if "%BECHO%"=="n" echo off
+
+
+set CC=cl -nologo -MD
+set CF=-O2 -GF -W4 %CFI%
+set LF=%BLIB%
+
+%CC% %CF% -c @b\src.rsp
+@if errorlevel 1 goto error1
+link -lib -nologo -out:%BLIB% @b\win64\vc.rsp
+@if errorlevel 1 goto error2
+
+%CC% %CF% examples\simple.c %LF%
+@if errorlevel 1 goto error3
+%CC% %CF% examples\uclpack.c %LF%
+@if errorlevel 1 goto error4
+
+
+@call b\done.bat
+@goto end
+
+goto end
+
+:error1
+@echo ERROR during compilation of source files!
+@goto end
+
+:error2
+@echo ERROR during linking of static library!
+@goto end
+
+:error3
+@echo ERROR during compilation of examples\simple.c!
+@goto end
+
+:error4
+@echo ERROR during compilation of examples\uclpack.c!
+@goto end
+
+:end
+@call b\unset.bat
